a888e7752e899cf5429c660a0bf41f634902a9d6
EDDY <eddmunyiri@gmail.com>
2023-10-22 17:07:56 +0300

    More static files updates

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,9 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 
-def posts(requests):
-    pass
+def posts(request):
+    return render(request, "blog/all-posts.html")
 
 
-def post_detail(requests):
+def post_detail(request):
     pass
